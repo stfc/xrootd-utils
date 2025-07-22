fbabef39a869f392248436665662b52196b47968
Jo-stfc <71326101+Jo-stfc@users.noreply.github.com>
2025-07-22 17:12:04 +0100

    remove jump host on connection

--- rollingreboot.sh
+++ rollingreboot.sh
@@ -1,17 +1,16 @@
 hosts=`cat hosts.txt`
 managers=`cat managers.txt`
-fedid=''
 for i in $hosts
 do
 echo "$i.gridpp.rl.ac.uk"
         for j in $managers
 	do
-	ssh -i .ssh/id_rsa -J ${fedid}@lcgui05.gridpp.rl.ac.uk root@${j}.gridpp.rl.ac.uk "echo \"$i.gridpp.rl.ac.uk\" >> /etc/xrootd/cms.blacklist"
+	ssh -i .ssh/id_rsa root@${j}.gridpp.rl.ac.uk "echo \"$i.gridpp.rl.ac.uk\" >> /etc/xrootd/cms.blacklist"
         done 
 	sleep 120
-	ssh -i .ssh/id_rsa -J ${fedid}@lcgui05.gridpp.rl.ac.uk root@${i}.gridpp.rl.ac.uk "reboot"
+	ssh -i .ssh/id_rsa root@${i}.gridpp.rl.ac.uk "reboot"
         for j in $managers                                                                     
 	do
-        ssh -i .ssh/id_rsa -J ${fedid}@lcgui05.gridpp.rl.ac.uk root@${j}.gridpp.rl.ac.uk "sed -i '$ d' /etc/xrootd/cms.blacklist"
+        ssh -i .ssh/id_rsa root@${j}.gridpp.rl.ac.uk "sed -i '$ d' /etc/xrootd/cms.blacklist"
         done
 done

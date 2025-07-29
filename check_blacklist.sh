5977822aa58ec9c136271012ca756d7d865c69bc
Jo-stfc <71326101+Jo-stfc@users.noreply.github.com>
2025-07-29 16:17:17 +0100

    Create check_blacklist.sh

---
+++ check_blacklist.sh
@@ -0,0 +1,18 @@
+managers=`cat managers.txt`
+for i in $managers
+do
+ssh -i .ssh/id_rsa root@${i}.gridpp.rl.ac.uk "cat /etc/xrootd/cms.blacklist" > ${i}.man.blk
+done
+files=(*.man.blk)
+first="${files[0]}"
+for f in $files;
+do
+  cmp --silent "$first" "$f" || {
+    echo "Mismatched blacklist: $first vs $f"
+    diff $first $f
+    exit 1
+  }
+done
+cat $first
+cat $first > blacklist.txt
+rm -rf *.man.blk
